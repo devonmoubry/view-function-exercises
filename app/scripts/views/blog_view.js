69fda0994bf712514aa8fc33baf224a1358bf643
Devon Moubry <devon@moubry.com>
2017-03-29 16:50:16 -0500

    create a form with HTML in view function

--- app/scripts/views/blog_view.js
+++ app/scripts/views/blog_view.js
@@ -1,5 +1,19 @@
 export default function blogMaker() {
   console.log('this export thing works');
 
+  let html = $(`
+     <div id="blogContainer">
+     <form id="blog-form">
+     <label for="blogTitle">title</label>
+     <input type="text" value="Catchy Title" placeholder="Catchy Title" id="blogTitle">
+     <label for="blogBody">body</label>
+     <input type="text" value="Tell me story, do the voices" id="blogBody">
+     <label for="saveBlog">Click Here To Save</label>
+     <input type="submit" value="SAVE">
+     </form>
+     </div>
+   `);
 
+   
+   return html;
 };
